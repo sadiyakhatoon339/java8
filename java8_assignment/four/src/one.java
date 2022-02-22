39f7072bb90c80cdf15e15941fe8db19243f7515
sadiyakhatoon339 <72649215+sadiyakhatoon339@users.noreply.github.com>
2022-02-22 11:25:20 +0530

    Update one.java

--- java8_assignment/four/src/one.java
+++ java8_assignment/four/src/one.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class one {
+public class One {
 
     public static void main(String[] args) {
         List<Integer> l= Arrays.asList(1,2,3,4,5,7,8,9,10);
