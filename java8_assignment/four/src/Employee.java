86e7e4696bf3c5ec787cdbb6db40c666520f8c38
sadiyakhatoon339 <72649215+sadiyakhatoon339@users.noreply.github.com>
2022-02-22 07:37:30 +0530

    Update Employee.java

--- java8_assignment/four/src/Employee.java
+++ java8_assignment/four/src/Employee.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Employee {
     String fullname;
@@ -12,26 +14,42 @@ public class Employee {
          this.city=city;
     }
 
+    public long getSalary(){
+        return salary;
+    }
+    public String getFullname(){
+        return fullname;
+    }
+
+    public String getCity(){
+        return city;
+    }
 
 }
 
 
  class Q4{
      public static void main(String[] args) {
-        Employee emp=new Employee("Ayeza Khan", 70000, "Delhi");
-         Employee emp1=new Employee("Hiba Bukhari", 30000, "mumbai");
-         Employee emp2=new Employee("Danish Taimoor", 710000, "Delhi");
-         Employee emp3=new Employee("Ayeza Taimoor", 7000, "Bangalore");
-         Employee emp4=new Employee("Ayeza Khatoon", 79000, "kolkata");
-         Employee emp5=new Employee("Sheheryar Munawar", 90000, "Chennai");
+         Employee emp=new Employee("Ayeza Ahmad Khan", 700, "Delhi");
+         Employee emp1=new Employee("Hiba Qadri Bukhari", 30000, "mumbai");
+         Employee emp2=new Employee("Danish Ahmad Taimoor", 800, "Delhi");
+         Employee emp3=new Employee("Ayeza Noor Taimoor", 7000, "Bangalore");
+         Employee emp4=new Employee("Ayeza Shah Khatoon", 79000, "kolkata");
+         Employee emp5=new Employee("Sheheryar Alam Munawar", 90000, "Chennai");
 
-         List<Employee> e=new ArrayList<>();
+         List<Employee> e=new ArrayList<Employee>();
          e.add(emp);
          e.add(emp1);
          e.add(emp2);
          e.add(emp3);
          e.add(emp4);
          e.add(emp5);
-         
+
+         System.out.println(e.stream()
+                 .filter(salary -> salary.getSalary()<5000)
+                 .filter(city->city.getCity().equals("Delhi"))
+                 .map(name->name.getFullname().split(" ")[0]).collect(Collectors.toList()));
+
+
      }
 }
