617c08b7dc12e9d2981c23e1ca8cfac0b9a056e3
sadiyakhatoon339 <72649215+sadiyakhatoon339@users.noreply.github.com>
2022-02-22 11:44:14 +0530

    Update four.java

--- java8_assignment/one/src/four.java
+++ java8_assignment/one/src/four.java
@@ -1,11 +1,11 @@
-interface in{
+interface Inter{
     String met(String s);
 }
 
 class Four{
     public static void main(String args[]){
         String r="danger";
-        in ob=(String s) ->s.toUpperCase();
+        Inter ob=(String s) ->s.toUpperCase();
         String ans=ob.met(r);
         System.out.println(ans);
     }
