4d5d6209efdabd4e1fbf906659126c48c6fefaad
sadiyakhatoon339 <72649215+sadiyakhatoon339@users.noreply.github.com>
2022-02-22 11:35:18 +0530

    Update three.java

--- java8_assignment/one/src/three.java
+++ java8_assignment/one/src/three.java
@@ -1,13 +1,13 @@
-interface gh{
+interface Go{
     String conc(String a,String b);
 }
 
-public class three {
+public class Three {
     public static void main(String args[]){
         String s1="Sadiya";
         String s2=" Khatoon";
 
-        gh ob=(String a,String b)  ->a.concat(b);
+        Go ob=(String a,String b)  ->a.concat(b);
         String k=ob.conc(s1,s2);
         System.out.println(k);
     }
